f7a1b5dddbd042d6caa9e96e9f2968e738ecd526
Rajveer188 <veerkajle188@gmail.com>
2025-03-03 23:18:07 +0530

    [Rajveer] Add. js program to validate 6 digit pin code

---
+++ PincodeValidator.js
@@ -0,0 +1,10 @@
+//regex to validate pincode
+const regex = /^[0-9]{6}/;
+
+//function to validate pin code
+function validate(pincode){
+    return regex.test(pincode);
+}
+
+console.log(validate(400088));
+
